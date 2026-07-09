de0e12391c391e3c9052db8d4def23c3f191c510
rahul_v <rahulv1904@gmail.com>
2026-07-09 08:23:10 +0530

    updated files

--- Learn_Docker/app.py
+++ Learn_Docker/app.py
@@ -1,76 +1,23 @@
-# from flask import Flask, request 
+from flask import Flask, request 
 
-# app = Flask(__name__)
+app = Flask(__name__)
+# my name is rahul 
 
-# # www.ownwebsite.com/even_odd
-# # www.google.com/mail
-# # www.google.com/drive
-# @app.route("/" , methods = ["GET"])
-# def welcome():
-#     return " Welcome to EVEN or ODD application "
+# www.google.com/mail
+@app.route("/" , methods = ["GET"])  # www.google.com
+def welcome():
+    return " This is Home Page "
 
-# @app.route("/even_odd", methods=["GET"])
-# def even_odd():
-#     num = request.args.get("num")
-#     num = int(num)
-#     if num % 2 == 0 :
-#         return f"{num} is Even"
-#     else :
-#         return f"{num} is Odd"
-
-# if __name__ == "__main__":
-#     app.run(host = "0.0.0.0", port = 5000)
-
-
-
-
-def even_odd(n):
-    if n % 2 == 0 :
-        print("even")
+@app.route("/even_odd", methods=["GET"]) # www.google.com/main
+def even_odd():
+    num = request.args.get("num")
+    num = int(num)
+    if num % 2 == 0 :
+        return f"{num} is Even"
     else :
-        print("odd")
+        return f"{num} is Odd"
 
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# laptop 1 - 20,000 Digital Ports 
